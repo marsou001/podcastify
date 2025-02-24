3bebe9c9e32d8c43f208b9ed3ca420f940f29a64
marouane <marsou431@gmail.com>
2025-02-24 14:16:47 +0000

    create podcast card component

---
+++ components/PodcastCard.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+type PodcastCardProps = {
+  podcastId: number;
+  title: string;
+  imgURL: string;
+  description: string;
+}
+
+function PodcastCard({ podcastId, title, imgURL, description}: PodcastCardProps) {
+  return (
+    <div className="cursor-pointer">
+      <figure className="flex flex-col gap-2">
+        <Image className="border-2 border-red-500" src={imgURL} alt={title} width={174} height={174} />
+        <div className="flex flex-col">
+          <h1 className="text-white-1 text-16 font-bold truncate">{ title }</h1>
+          <h2 className="text-white-4 text-12 font-normal truncate capitalize">{ description }</h2>
+        </div>
+      </figure>
+    </div>
+  )
+}
+
+export default PodcastCard;
