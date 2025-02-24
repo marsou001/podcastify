f37bd866de6f3f6a14f0a05902ca7cad687ce840
marouane <marsou431@gmail.com>
2025-02-24 14:24:37 +0000

    enhance podcast card thumbnail design

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -11,7 +11,13 @@ function PodcastCard({ podcastId, title, imgURL, description}: PodcastCardProps)
   return (
     <div className="cursor-pointer">
       <figure className="flex flex-col gap-2">
-        <Image className="border-2 border-red-500" src={imgURL} alt={title} width={174} height={174} />
+        <Image
+          src={imgURL}
+          alt={title}
+          width={174}
+          height={174}
+          className="aspect-square h-fit w-full rounded-xl 2xl:size-[200px]"
+        />
         <div className="flex flex-col">
           <h1 className="text-white-1 text-16 font-bold truncate">{ title }</h1>
           <h2 className="text-white-4 text-12 font-normal truncate capitalize">{ description }</h2>
