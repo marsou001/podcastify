93a7211b9ab8530a489ed36cf8c45fd8315ed4f6
marouane <marsou431@gmail.com>
2025-02-24 19:03:06 +0000

    import ClerkProvider and useAuth from @clerk/nextjs instead of @clerk/clerk-react

--- app/providers/ConvexClerkProvider.tsx
+++ app/providers/ConvexClerkProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ReactNode } from "react";
@@ -9,7 +9,19 @@ const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as strin
 
 function ConvexClerkProvider({ children }: { children: ReactNode }) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!} appearance={{
+      layout: {
+        socialButtonsVariant: "iconButton",
+        logoImageUrl: "/icons/auth-logo.svg",
+      },
+      variables: {
+        colorText: "white",
+        colorBackground: "#15171c",
+        colorInputText: "white",
+        colorInputBackground: "#1b1f29",
+        colorPrimary: "",
+      }
+    }}>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
       </ConvexProviderWithClerk>
