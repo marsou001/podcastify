70641d95fb53064a67a3891ee4c354db8eead7d5
marouane <marsou431@gmail.com>
2025-02-24 18:06:02 +0000

    set up auth with Convex and Clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://lasting-tetra-82.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
