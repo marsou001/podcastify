cf9abc15e04613cb13e1b84d2b4d74784ffb2c68
marouane <marsou431@gmail.com>
2025-03-11 01:12:46 +0000

    remove redundant comment

--- convex/http.ts
+++ convex/http.ts
@@ -52,7 +52,6 @@ http.route({
 const validateRequest = async (
   req: Request
 ): Promise<WebhookEvent | undefined> => {
-  // TODO: get your Clerk webhook secret
   const webhookSecret = process.env.CLERK_WEBHOOK_SECRET!;
   if (!webhookSecret) {
     throw new Error("CLERK_WEBHOOK_SECRET is not defined");
