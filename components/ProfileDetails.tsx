290deb479befb64ad2fbe3fdcad3d02dba98239c
marouane <marsou431@gmail.com>
2025-03-24 14:59:04 +0000

    show not found message if profile does not exist

--- components/ProfileDetails.tsx
+++ components/ProfileDetails.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
-import { AudioProps } from "@/types";
+import { AudioProps, PodcastProps } from "@/types";
 import { Loader } from "lucide-react";
 import EmptyState from "./EmptyState";
 import ProfileInfo from "./ProfileInfo";
@@ -11,13 +11,19 @@ import ProfilePodcasts from "./ProfilePodcasts";
 
 function ProfileDetails({ profileId }: { profileId: string }) {
   const [randomPodcast, setRandomPodcast] = useState<AudioProps>({ podcastId: "", title: "", author: "", audioURL: "", imageURL: "" });
+  const [podcasts, setPodcasts] = useState<PodcastProps[]>([]);
+  const [views, setViews] = useState(0);
   const profile = useQuery(api.users.getUserByClerkId, { clerkId: profileId });
   const podcastsData = useQuery(api.podcasts.getPodcastByAuthorId, { authorId: profileId });
-  const podcasts = podcastsData?.podcasts!;
-  const views = podcastsData?.listeners!;
 
   useEffect(() => {
     if (podcastsData === undefined) return;
+    const { podcasts, listeners } = podcastsData;
+    if (podcasts.length === 0) return;
+
+    setPodcasts(podcasts);
+    setViews(listeners);
+
     const randomPodcast = podcasts[Math.floor(Math.random() * podcasts.length)];
     setRandomPodcast({
       podcastId: randomPodcast._id,
