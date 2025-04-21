de6b270cd4bf52dccbb01163659d44425332934e
marouane <marsou431@gmail.com>
2025-04-21 03:02:36 +0100

    make clerk elements more visible

--- app/providers/ConvexClerkProvider.tsx
+++ app/providers/ConvexClerkProvider.tsx
@@ -4,12 +4,14 @@ import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ReactNode } from "react";
+import { dark } from "@clerk/themes";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 function ConvexClerkProvider({ children }: { children: ReactNode }) {
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!} appearance={{
+      baseTheme: dark,
       layout: {
         socialButtonsVariant: "iconButton",
         logoImageUrl: "/icons/auth-logo.svg",
@@ -20,7 +22,7 @@ function ConvexClerkProvider({ children }: { children: ReactNode }) {
         colorInputText: "white",
         colorInputBackground: "#1b1f29",
         colorPrimary: "",
-      }
+      },
     }}>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
